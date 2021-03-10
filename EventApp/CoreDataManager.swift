6effafa40d940f67acce615c71e481e5c907ddd7
guillemsole <guillemsolecubilo@gmail.com>
2021-03-10 13:00:32 +0100

    REORDER files into folders ADD CoreData Model and Manager

---
+++ EventApp/CoreDataManager.swift
@@ -0,0 +1,48 @@
+//
+//  CoreDataManager.swift
+//  EventApp
+//
+//  Created by Guillem Solé Cubilo on 10/3/21.
+//
+
+import CoreData
+import UIKit
+
+final class CoreDataManager {
+    lazy var persistentContainer: NSPersistentContainer = {
+        let persistentContainer = NSPersistentContainer(name: "EventsApp")
+        persistentContainer.loadPersistentStores { _, error in
+            print(error?.localizedDescription ?? "")
+        }
+        return persistentContainer
+    }()
+    
+    var moc: NSManagedObjectContext {
+        persistentContainer.viewContext
+    }
+    
+    func saveEvent(name: String, date: Date, image: UIImage) {
+        let event = Event(context: moc)
+        event.setValue(name, forKey: "name")
+        event.setValue(date, forKey: "date")
+        let imageData = image.jpegData(compressionQuality: 1)
+        event.setValue(imageData, forKey: "image")
+        
+        do {
+            try moc.save()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func fetchEvents() -> [Event] {
+        do {
+            let fetchRequest = NSFetchRequest<Event>(entityName: "Event")
+            let event = try moc.fetch(fetchRequest)
+            return event
+        } catch {
+            print(error)
+            return []
+        }
+    }
+}
