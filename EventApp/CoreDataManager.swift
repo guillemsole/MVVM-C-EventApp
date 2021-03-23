b1df34837463590ab1a607efc9805ae37c814d17
guillemsole <guillemsolecubilo@gmail.com>
2021-03-23 11:08:34 +0100

    REMOVE comments

--- EventApp/CoreDataManager.swift
+++ EventApp/CoreDataManager.swift
@@ -49,42 +49,4 @@ final class CoreDataManager {
             print(error.localizedDescription)
         }
     }
-    
-//    func updateEvent(event: Event, name: String, date: Date, image: UIImage) {
-//        event.setValue(name, forKey: "name")
-//        event.setValue(date, forKey: "date")
-//
-//        let resizedImage = image.sameAspectRatio(newHeight: 250)
-//        let imageData = resizedImage.jpegData(compressionQuality: 0.5)
-//        event.setValue(imageData, forKey: "image")
-//
-//
-//    }
-//
-//    func saveEvent(name: String, date: Date, image: UIImage) {
-//        let event = Event(context: moc)
-//        event.setValue(name, forKey: "name")
-//        event.setValue(date, forKey: "date")
-//
-//        let resizedImage = image.sameAspectRatio(newHeight: 250)
-//        let imageData = resizedImage.jpegData(compressionQuality: 0.5)
-//        event.setValue(imageData, forKey: "image")
-//
-//        do {
-//            try moc.save()
-//        } catch {
-//            print(error.localizedDescription)
-//        }
-//    }
-//
-//    func fetchEvents() -> [Event] {
-//        do {
-//            let fetchRequest = NSFetchRequest<Event>(entityName: "Event")
-//            let event = try moc.fetch(fetchRequest)
-//            return event
-//        } catch {
-//            print(error)
-//            return []
-//        }
-//    }
 }
